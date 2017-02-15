ec31cce6d066aad9f3d8003652e7b0cb66b97d84
kuanyu <starmap0312@gmail.com>
2017-02-15 17:01:45 +0800

    add more

--- TrySuccessFailure.scala
+++ TrySuccessFailure.scala
@@ -1,10 +1,9 @@
 import scala.util.{Try, Success, Failure}
-//import java.io.Exception
 
 object TrySuccessFailure {
 
     def bad(num: Int): Try[Int] = {
-        Try(
+        Try(                                 // return object will be wrapped in Success or Failure
             if (num == 3) {
                 num                          // return Success[Int]
             } else {
@@ -14,13 +13,14 @@ object TrySuccessFailure {
     }
 
     def main(args: Array[String]) {
+        // Try[T]/Success[T] extends Try[T]/Failure[T] extends Try[T]
         bad(3) match {
             case Success(num) => println(s"Success: $num")
-            case Failure(e)   => println(s"Failure: ${e.getClass}")
+            case Failure(ex)   => println(s"Failure: ${ex.getClass}")
         }
         bad(4) match {
             case Success(num) => println(s"Success: $num")
-            case Failure(e)   => println(s"Failure: ${e.getClass}")
+            case Failure(ex)   => println(s"Failure: ${ex.getClass}")
         }
     }
 }
