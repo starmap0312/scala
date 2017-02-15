d546d8c15bc4b7474bb8225ae29c1163e0fe7b39
kuanyu <starmap0312@gmail.com>
2017-02-15 13:20:27 +0800

    add more

--- Utilities.scala
+++ Utilities.scala
@@ -1,16 +1,19 @@
-import scala.io.{Source}
+import scala.io.{Source, StdIn}
 
 object Utilities {
     def main(args: Array[String]) {
-        // 1) read line from file
-        // 1.1)
+        // 0) StdIn.readLine(): read line from STDIN
+        println(StdIn.readLine())
+
+        // 1) Source.fromFile([filename]): read line from file
+        // 1.1) use getLines() to fetch lines
         for (line <- Source.fromFile("data.txt").getLines()) {
             println(line)
         }
-        // 1.2)
+        // 1.2) print out the file content
         Source.fromFile("data.txt").foreach(print)
 
-        // 2) read from URL
+        // 2) Source.fromURL([url]): read from URL
         val html = Source.fromURL("http://www.example.com").mkString
         println(html)
     }
