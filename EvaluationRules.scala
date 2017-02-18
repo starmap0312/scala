7174a6b20d69df8ec19770a7240c1454f3245fa4
kuanyu <starmap0312@gmail.com>
2017-02-18 11:09:02 +0800

    add more

--- EvaluationRules.scala
+++ EvaluationRules.scala
@@ -2,12 +2,77 @@
 
 object EvaluationRules {
     def main(args: Array[String]) {
-        def example = 2      // evaluated only when called (lazy evaluation)
-        val example = 2      // evaluated immediately
-        lazy val example = 2 // evaluated once when needed
+        // 1) variable definition
+        // 1.1) evaluated only when called (lazy evaluation)
+        def example1 = 2
+        // 1.2) evaluated immediately
+        val example2 = 2
+        // 1.3) evaluated once when needed (lazy evaluation and evaluated only once)
+        lazy val example3 = 2
+        println(example1)
+        println(example2)
+        println(example3)
 
-        def square(x: Double)    // call by value
-        def square(x: => Double) // call by name
-        def myFct(bindings: Int*) = { ... } // bindings is a sequence of int, containing a varying # of arguments
+        // 2) function definition
+        // 2.1) def [function]: evaluated when called (lazy evaluation, this creates new function every time called)
+        //    syntax:
+        //    def functionName: [function type] = { 
+        //        function body
+        //        [expr]
+        //    }
+        // 2.2) val [function]: evaluated when defined (evaluated immediately)
+        //    syntax:
+        //    val functionName: [function type] = { 
+        //        function body
+        //        [expr]
+        //    }
+        // 2.3) lazy val [function]: evaluated when called the first time (lazy evaluation and evaluated only once) 
+        //    syntax:
+        //    lazy val functionName: [function type] = { 
+        //        function body
+        //        [expr]
+        //    }
+        // example 1
+        def even1     : (Int => Boolean) = (_ % 2 == 0)
+        val even2     : (Int => Boolean) = (_ % 2 == 0)
+        lazy val even3: (Int => Boolean) = (_ % 2 == 0)
+        println("even1(1) returns " + even1(1))
+        println("even2(1) returns " + even2(1))
+        println("even3(1) returns " + even3(1))
+        // example 2
+        // get new result every time called (as this is lazy evaluation)
+        def rand1: (() => Int) = {
+            val result = util.Random.nextInt
+            () => result
+        }
+        println("rand1() 1st call: " + rand1())
+        println("rand1() 2nd call: " + rand1())
+
+        // get same result every time called (as the value is already evaluated when defined)
+        val rand2: (() => Int) = {
+            val result = util.Random.nextInt
+            () => result
+        }
+        println("rand2() 1st call: " + rand2())
+        println("rand2() 2nd call: " + rand2())
+        // note: the above definition is different from the following
+        // val rand2: (() => Int) = (() => util.Random.nextInt)
+
+        // get same result every time called (but this is also lazy evaluation when first time called)
+        lazy val rand3: (() => Int) = {
+            val result = util.Random.nextInt
+            () => result
+        }
+        println("rand3() 1st call: " + rand3())
+        println("rand3() 2nd call: " + rand3()) 
+        
+
+        // 3) function arguments evaluation: call by value vs. call by name
+        // 3.1) call by value: evaluates the function arguments before calling the function
+        def square1(x: Double) = {}
+        // 3.2) call by name : evaluates the function first, and then evaluates the arguments if needed (lazy evaluation)
+        def square2(x: => Double) = {} 
+        // 3.3) define a sequence of int, containing a varying # of arguments
+        def func(arg: Int*) = {}
     }
 }
--- NamedFunction.scala
+++ NamedFunction.scala
@@ -1,89 +1,40 @@
 // Scala supports first-class functions
 //   functions can be expressed in function literal syntax
 //   functions can be represented by objects (called function values)
-// conventional use
+// conventional
 // 0) normal function definition
 //    syntax:
 //    def functionName([list of parameters]): [return type] = {
 //            return [expr] 
 //        }
+//    use {} to define multiple statements
+//    ({} indicates a block of code composed of multiple statements and its value is that of the last statement)
 // 1) shorthand to nullary or unary function definition
 //    syntax:
-//    def/val/lazy val functionName: [function type] = [expr]
-//    use with {} to define functions:
-//      {} indicates a block of code composed of multiple statements and its value is that of the last statement
-// 1.1) def [function]: it evaluates only when called (lazy evaluation, this creates new function every time called)
-//    syntax:
-//    def functionName: [function type] = { 
-//        function body
-//        [expr]
-//    }
-// 1.2) val [function]: it evaluates when defined (evaluated immediately)
-//    syntax:
-//    val functionName: [function type] = { 
-//        function body
-//        [expr]
-//    }
-// 1.3) lazy val [function]: it evaluates when called the first time (lazy evaluation and evaluated only once) 
-//    syntax:
-//    lazy val functionName: [function type] = { 
-//        function body
-//        [expr]
-//    }
+//    def/val/lazy val functionName: [return type] = [expr/expr with _ variable]
+//    (note: _ indicates the anonymous single argument)
 
 object NamedFunciton {
     def main(args: Array[String]) {
-        // example 0:
-        // 0) normal function defintion 
-        def addInt(a:Int, b:Int): Int = {
-            return a + b
+        // normal function defintion 
+        def add(a:Int, b:Int): Int = {
+            return a + b                // return can be omitted
         }
-        println("addInt(1, 2) = " + addInt(1, 2))
+        println("1 + 2 = " + add(1, 2)) // a new function object add(1, 2) is created when called
 
-        // example 1: shorthand to unary function definition
-        def even1     : (Int => Boolean) = (_ % 2 == 0)
-        val even2     : (Int => Boolean) = (_ % 2 == 0)
-        lazy val even3: (Int => Boolean) = (_ % 2 == 0)
+        // use of {} block
+        def ten: Int = {
+            println("statement 1")
+            println("statement 2")
+            return 10
+        }
+        println("ten = " + ten)         // a new variable object ten is created when called
+
+        // shorthand to unary function definition (explicit argument names can be omitted)
+        def even1(x: Int): Boolean = (x % 2 == 0)
+        def even2: (Int => Boolean) = (_ % 2 == 0)
         println("even1(1) returns " + even1(1))
         println("even2(1) returns " + even2(1))
-        println("even3(1) returns " + even3(1))
-
-        // example 1: shorthand to nullary function definition
-        // 1.1) def function: get new result every time called
-        def rand1: (() => Int) = {
-            val result = util.Random.nextInt
-            () => result
-        }
-        println("rand1() 1st call: " + rand1())
-        println("rand1() 2nd call: " + rand1())
-
-        // 1.2) val function: get same result every time called
-        val rand2: (() => Int) = {
-            val result = util.Random.nextInt
-            () => result
-        }
-        println("rand2() 1st call: " + rand2())
-        println("rand2() 2nd call: " + rand2())
-        // note: the above definition is different from the following
-        // val rand2: (() => Int) = (() => util.Random.nextInt)
-
-        // 1.3) lazy val function: get same result every time called
-        lazy val rand3: (() => Int) = {
-            val result = util.Random.nextInt
-            () => result
-        }
-        println("rand3() 1st call: " + rand3())
-        println("rand3() 2nd call: " + rand3())
-
-        // example 3: unary function definition with pattern matching
-        def printType: ((Any) => Unit) = {
-            case x: Int    => println("Int")
-            case y: String => println("String")
-            case _         => println("Other")
-        }
-        printType(1)
-        printType("two")
-        printType(3.0)
     }
 }
 
