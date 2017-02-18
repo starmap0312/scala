19bc883184792a01b7d68a43351830527e6fc8d0
kuanyu <starmap0312@gmail.com>
2017-02-18 16:15:28 +0800

    add more

--- CaseClass1.scala
+++ CaseClass1.scala
@@ -19,29 +19,32 @@ object CaseClass {
     // 1) immutable fields (constructor arguments)
     // 2) equals(), hashCode(), and toString() methods are automatically defined
 
-    case object DoWork                        // the constuctor accepts no argument
-    case object NoWork                        // the constuctor accepts no argument
+    trait Work
+    case object DoWork extends Work                   // the constuctor accepts no argument
+    case object NoWork extends Work                   // the constuctor accepts no argument
     // case object implies: singleton object with synthetic suger similar to case class
 
     def main(args: Array[String]) {
         // example 1: pattern matching objects of case class
+        def matchPerson(person: Person): Unit = person match {
+            case Person("Alice", 25) => println("value pattern matched: Person(Alice, 25)")
+            case Person(  "Bob", 30) => println("value pattern matched: Person(  Bob, 30)")
+            case Person(      x,  y) => println("typed pattern matched: Person(    x,  y), where (x, y) = (" + x + ", " + y + ")")
+        }
         val alice   = new Person("Alice",   25)       // create a new object using the case class
         val bob     = new Person("Bob",     25)       // create a new object using the case class
         val charlie = new Person("Charlie", 25)       // create a new object using the case class
         for (person <- List(alice, bob, charlie)) {
-            person match {
-                case Person("Alice", 25) => println("value pattern matched: Person(Alice, 25)")
-                case Person(  "Bob", 30) => println("value pattern matched: Person(  Bob, 30)")
-                case Person(      x,  y) => println("typed pattern matched: Person(    x,  y), where (x, y) = (" + x + ", " + y + ")")
-            }
+            matchPerson(person)
         }
 
         // example 2: pattern matching case object (singleton)
+        val matchWork: (Work => Unit) = {
+            case DoWork => println("value pattern matched: DoWork")
+            case NoWork => println("value pattern matched: NoWork")
+        }
         for (work <- List(DoWork, NoWork)) {
-            work match {
-                case DoWork => println("DoWork")
-                case NoWork => println("NoWork")
-            }
+            matchWork(work)
         }
     }
 }
--- PatternMatching.scala
+++ PatternMatching.scala
@@ -7,7 +7,7 @@
 
 object PatternMatching {
     // match value & typed pattern
-    def matchFunc1(x: Any): Any = x match {
+    def matchNumber(x: Any): Any = x match {
         case 1         => "value pattern matched: " + 1 
         case "two"     => "value pattern matched: " + "two"
         case x: Int    => "typed pattern matched: " + "x: Int" 
@@ -16,16 +16,28 @@ object PatternMatching {
     }
 
     // match list
-    def matchFunc2(x: List[Int]) = x match {
-        case Nil => println("Nil")
+    def matchList[T](list: List[T]) = list match {
+        case Nil        => ("value pattern matched: Nil")        // the empty list object
+        case x::Nil     => ("typed pattern matched: x::Nil")     // a single-element list 
+        case List(x)    => ("typed pattern matched: List(x)")    // a single-element list (same as above)
+        case 1::2::cs   => ("typed pattern matched: 1::2::cs")   // a list object starting with 1 then 2 
+        case x::xs      => ("typed pattern matched: x::xs")      // at-least-one-element list
+        case _          => ("nothing matched")
     }
 
     def main(args: Array[String]) {
-        // example 2: match value & typed pattern
-        println(matchFunc1(1))
-        println(matchFunc1("two"))
-        println(matchFunc1(3))
-        println(matchFunc1(4.0))
-        println(matchFunc1("N/A"))
+        // example 1: match value & typed pattern
+        println(matchNumber(1))
+        println(matchNumber("two"))
+        println(matchNumber(3))
+        println(matchNumber(4.0))
+        println(matchNumber("N/A"))
+
+        // example 2: match List object
+        println(matchList(Nil))
+        println(matchList(List(1)))
+        println(matchList(List(1, 2)))
+        println(matchList(List(3, 4)))
+        println(matchList(List(5, 6, 7, 8, 9)))
     }
 }
