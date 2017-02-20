21c68619fe00a0ca807c027310e14e240001d38c
kuanyu <starmap0312@gmail.com>
2017-02-20 09:48:11 +0800

    add more

--- ForComprehensions.scala
+++ ForComprehensions.scala
@@ -2,41 +2,55 @@
 // 1) [element] <- [collection] : a generator
 // 2) [generator] if [condition]: a filter
 // 3) multiple generators is conceptually similar to a nested loop (first/outmost generator varies slower)
-// 4) for (x <- e1; y <- e2 if f; z <- e3) yield
+// 4) for (x <- e1; y <- e2 if f; z <- e4) yield {}
 //    can be written as:
 //    for {
 //        x <- e1
 //        y <- e2 if f
-//        z <- e3
-//    } yield
+//        z <- e4
+//    } yield {}
 // 5) for (x <- e1) yield e2
 //    is translated to:
 //    e1.map(x => e2)
 // 6) for (x <- e1 if f) yield e2
 //    is translated to:
 //    for (x <- e1.filter(x => f)) yield e2
-// 7) for (x <- e1; y <- e2) yield e3
+// 7) for (x <- e1; y <- e2) yield e4
 //    is translated to:
-//    e1.flatMap(x => for (y <- e2) yield e3)
-// 8) for-comprehension can be used for any container type, as long as its map(), flatMap() and filter() are defined
+//    e1.flatMap(x => for (y <- e2) yield e4)
+// 8) for-comprehension can be used for any type whose map(), flatMap() and filter() are defined
 
 object ForComprehensions {
     def main(args: Array[String]) {
-        // ex1.
+        // ex1. for/yield construct
         def e1 = for (x <- 1 to 2; y <- -2 to -1) yield (x, y)
-        // is equivalent to
-        def e2 = (1 to 2).flatMap(x => (-2 to -1).map(y => (x, y)))
-        for (x <- e1) println(x)
-        for (x <- e2) println(x)
+        // is equivalent to:
+        def e2 = (1 to 2).flatMap(
+            x => for (y <- -2 to -1) yield (x, y)
+        )
+        // is equivalent to:
+        def e3 = (1 to 2).flatMap(
+            x => (-2 to -1).map(y => (x, y))
+        )
+        for (x <- e1) print(x)
+        println()
+        for (x <- e2) print(x)
+        println()
+        for (x <- e3) print(x)
+        println()
 
-        // ex2.
-        def e3 = for {  
+        // ex2. for/yield construct with filters
+        def e4 = for {  
             i <- 1 until 3
             j <- -5 until i if (j % 2 == 0)
         } yield (i, j)  
         // is equivalent to
-        def e4 = (1 until 3).flatMap(i => (-5 until i).filter(j => (j % 2 == 0)).map(j => (i, j)))
-        for (x <- e3) println(x)
+        def e5 = (1 until 3).flatMap(
+            i => (-5 until i).filter(j => (j % 2 == 0)).map(
+                j => (i, j)
+            )
+        )
         for (x <- e4) println(x)
+        for (x <- e5) println(x)
     }
 }
