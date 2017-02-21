7efa6fa7e4300a117a8773f35075469e8c687f8c
kuanyu <starmap0312@gmail.com>
2017-02-21 14:28:37 +0800

    add more

--- Primitives.scala
+++ Primitives.scala
@@ -27,7 +27,13 @@ object Primitives {
         println(myVar)
         println(myVal)
 
-        // 2) Option[T]/None[T]/Some[T]: the latter two extend Option[T]:
+        // 2) String: length() and concat()
+        var palindrome = "Dot saw I was Tod"
+        println(palindrome.length())
+        println(palindrome.concat(" appended"))
+        println(palindrome + " appended")
+
+        // 3) Option[T]/None[T]/Some[T]: the latter two extend Option[T]:
         //    used to avoid problems you encounter when dealing with null values returned from functions
         // ex1.
         toInt("123") match {
@@ -53,21 +59,25 @@ object Primitives {
         println(getMapValue2(2))
         println(getMapValue2(3))
 
-        // 3) String: length() and concat()
-        var palindrome = "Dot saw I was Tod"
-        println(palindrome.length())
-        println(palindrome.concat(" appended"))
-        println(palindrome + " appended")
+        // 4) Either[A, B]/Left/Right: a container type, the latter two extend Either
+        //    used when need to deal with situations where the result can be of one of two possible types
+        def double: ((Int) => Either[Int, Double]) = {
+            case x: Int if x < 5  => Left(x * 2)
+            case x: Int if x >= 5 => Right(x * 2.0)
+        }
+        println(double(3))
+        println(double(6))
 
-        // 4) Stream: lazy list
+        // 5) Stream: lazy list
         val stream1 = 1#::2#::3#::Stream.empty
         val stream2 = (1 to 3).toStream         // use Stream to avoid java.lang.OutOfMemory error when creating huge lists
         stream1.foreach(println)
         stream2.foreach(println)
         val list = 1::2::3::Nil
         list.foreach(println)
-        // 4.1) Stream.cons([element], [stream]) returns a stream
+        // 5.1) Stream.cons([element], [stream]) returns a stream
         val stream3 = Stream.cons(0, Stream.cons(1, Stream.empty))   // cons returns the rest of the stream NOT another new stream
         stream3.foreach(println)
+
     }
 }
