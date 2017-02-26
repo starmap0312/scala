8bdd109ec3a1f8970f55f83ed7ad1bb67a0b1513
kuanyu <starmap0312@gmail.com>
2017-02-26 14:33:26 +0800

    add more

--- Erasure.scala
+++ Erasure.scala
@@ -8,7 +8,13 @@ object Erasure {
         case _ => None
     }
 
-    def filter2[T](list: List[Any])(implicit tag: ClassTag[T]) = list.flatMap {
+    def filter2[T](list: List[Any]) = list.flatMap {
+        case element: Object => Some(element) // warning: abstract type pattern T is unchecked since it is eliminated by erasure
+        // don’t expect compiler to know what T is when we get to pattern matching (since it will be erased by the JVM at runtime)
+        case _ => None
+    }
+
+    def filter3[T](list: List[Any])(implicit tag: ClassTag[T]) = list.flatMap {
         // compiler turns unchecked type tests in pattern matches into checked ones by wrapping a (_: T) type pattern as tag(_: T),
         //   where tag is the ClassTag[T] instance
         case element: T => Some(element)
@@ -22,23 +28,25 @@ object Erasure {
 
     def main(args: Array[String]) {
         val list = List(1, "string1", List(), "string2")
-        // problem: specify T == String and we expect that the pattern matching of filter() is able to figure out the type
-        //          however, in practice all generic types are erased after compilation (i.e. eliminated by erasure)
+        // problem: we expect the pattern matching of filter() can figure out the type T == String
+        //          however, generic types will be erased after compilation (i.e. eliminated by erasure)
         val result1 = filter1[String](list)
-        // T will be intepreted as its upperbound (in this case, Object)
-        // so the filter() method will not filter out String type as expected 
-        println(result1)                  // List(1, string1, List(), string2)
+        val result2 = filter2[String](list)
+        // T will be intepreted as its upperbound, i.e. Object, so filter() will not filter out String type as expected 
+        println(result1)                    // List(1, string1, List(), string2)
+        println(result2)                    // List(1, string1, List(), string2)
         // solution: when we require an implicit value that is of type ClassTag, compiler will create this value for us
         //           i.e. we need to ask compiler to provide us with an implicit instance of a needed ClassTag
         //                although we never needed to use the parameter itself
-        val result2 = filter2[String](list)
-        println(result2)                  // List(1, string1, List(), string2)
+        val result3 = filter3[String](list)
+        println(result3)                    // List(1, string1, List(), string2)
 
         // problem: Class tags cannot differentiate on a higher level
-        //          i.e. it can differentiate between sets and lists, but it cannot tell apart one list from another (ex. List[Int] vs List[String])
+        //          i.e. it can differentiate between sets and lists
+        //               but it cannot tell one list from another (ex. List[Int] vs List[String])
         val list2: List[List[Any]] = List(List(1, 2), List("a", "b"))
-        val result3 = filter2[List[Int]](list2)
-        println(result3)                  // List(List(1, 2), List(a, b))
+        val result4 = filter3[List[Int]](list2)
+        println(result4)                    // List(List(1, 2), List(a, b))
 
         // TypeTag provides runtime information about the type
         // ClassTag provides runtime information about the value
