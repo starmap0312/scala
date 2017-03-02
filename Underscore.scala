2e9d886858a50274d59c0e0df457a52583995626
kuanyu <starmap0312@gmail.com>
2017-03-02 09:47:55 +0800

    add more

--- Underscore.scala
+++ Underscore.scala
@@ -1,18 +1,27 @@
 object Underscore {
     def main(args: Array[String]) {
         // 1) Ignored variables
+        // 1.1) Ignored variables
         val _ = 5
-        // 2) Ignored parameters of anonymous functions
+        // 1.2) Ignored parameters of anonymous functions
         List(1, 2, 3).foreach(_ => println("Hi"))
-        // 3) Partially applied functions
-        List(1, 2, 3).foreach(println(_))
-        List(1, 2, 3).foreach(println _)
-        // 3) Wildcard imports
-        import java.util._
-        // 4) Wildcard patterns
-        Some(5) match {                     // val Some(_) = Some.apply(5)
+        // 1.3) Wildcard patterns
+        Some(5) match {                     // i.e. val Some(x) = Some.apply(5), where x is ignored and replaced with _
             case Some(_) => println("Yes")
         }
+        // 2) Placeholders
+        // 2.1) Partially applied functions
+        List(1, 2, 3).foreach(println(_))   // lambda expression: when writing lambda with no explicit argument type, Scala infers type from argument that foreach expects
+        List(1, 2, 3).foreach(              // so the above is equavalent to the following
+            { (x: Int) => println(x) }
+        )
+        List(1, 2, 3).foreach(println _)    // "process _" represents a function/method
+        // 2.2) Placeholder syntax
+        println(List(1, 2, 3).map(_ + 1))
+
+        // 3) Wildcards
+        // 3.1) Wildcard imports
+        import java.util._
         // 
     }
 }
