688853f79b1108c5d286fde1527f5a00b314dfb6
kuanyu <starmap0312@gmail.com>
2017-03-03 10:21:47 +0800

    add more

--- FileWriters.scala
+++ FileWriters.scala
@@ -2,13 +2,15 @@ import java.io.{File, PrintWriter, FileWriter, BufferedWriter}
 
 object FileWriters {
     def main(args: Array[String]) {
-        // 1) use PrintWriter
+        // 1) use PrintWriter: write() & append()
         val writer1 = new PrintWriter(new File("printwriter.txt"))
         writer1.write("Hello, world\n")
+        writer1.append("Hello, world\n")
         writer1.close
         // 2) use FileWriter
         val writer2 = new BufferedWriter(new FileWriter("filewriter.txt"))
         writer2.write("Hello, world\n")
+        writer2.append("Hello, world\n")
         writer2.close()
     }
 }
