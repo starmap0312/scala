26b279c98aee4a62b53db3e6dc540632fc9ce3d0
kuanyu <starmap0312@gmail.com>
2017-03-04 09:48:21 +0800

    add more

--- HigherOrderFunctions.scala
+++ HigherOrderFunctions.scala
@@ -1,7 +1,11 @@
-// take a function as a parameter or return functions
+// 1) treat function as a function parameter, or
+// 2) return a function
 object HigherOrderFunctions {
     def main(args: Array[String]) {
-        // function composition, i.e. g(f(x))
+        // 1) function composition: ex. applying function in sequence, ex. g.f(x) = g(f(x))
+        //    f  : X -> Y
+        //    g  : Y -> Z
+        //    g.f: X -> Z
         def f(x: Int): Int = (x * x) 
         def g(x: Int): Int = (x + 1) 
         def g_of_f1(x: Int): Int = g(f(x))
@@ -10,25 +14,29 @@ object HigherOrderFunctions {
         println(g_of_f1(3))
         println(g_of_f2(3))
         println(g_of_f3(3))
-        // pass in a callback function/lambda expression/return a function object
-        def sum_f(callback: Int => Int): ((Int, Int) => Int) = {  
+        // 2) functions that take function a parameter (callback), or return a function object
+        //    i.e. a function object that has other function as one of its characteristics, or
+        //         a function whose behavior is to produce another function object (like function factory)
+        def sum_f1(callback: Int => Int): ((Int, Int) => Int) = {  
+            // it takes a callback function of type (Int => Int) as parameter, and
+            // it returns a function of type (Int, Int) => Int
             def sum(a: Int, b: Int): Int = {
                 return callback(a) + callback(b)
             }
             return sum                           // returns a function object of type ((Int, Int) => Int) 
         } 
-        def sum_square = sum_f((x: Int) => x * x)
-        def sum_cube   = sum_f((x: Int) => x * x * x)
+        def sum_square = sum_f1((x: Int) => x * x)
+        def sum_cube   = sum_f1((x: Int) => x * x * x)
         println(sum_square(1, 2))
         println(sum_cube(1, 2))
    
-        // shorthand: define a function of type (Int => Int) => (Int, Int) => Int that works like the above 
-        // note: sum_f2 takes a callback function of type (Int => Int) as parameter and returns a function of type (Int, Int) => Int
-        def sum_f2(callback: Int => Int)(a: Int, b: Int): Int = {
+        // syntactic suger:
+        //   flattening the inner function in the above example, as it is not used outside and can be anonymous
+        def sum_f2(callback: Int => Int)(a: Int, b: Int): Int = { // a shorthand of defining the function in the above example
              return callback(a) + callback(b)
         }
+        println(sum_f1((x: Int) => x * x)(1, 2))
         println(sum_f2((x: Int) => x * x)(1, 2))
-        println(sum_f2((x: Int) => x * x * x)(1, 2))
     }
 }
 
--- Underscore.scala
+++ Underscore.scala
@@ -3,25 +3,32 @@ object Underscore {
         // 1) Ignored variables
         // 1.1) Ignored variables
         val _ = 5
+        val x = 5                                 // the above is equavalent to the following, and x is ignored
         // 1.2) Ignored parameters of anonymous functions
-        List(1, 2, 3).foreach(_ => println("Hi"))
+        List(1, 2).foreach(_ => println("Hi"))
+        List(1, 2).foreach(x => println("Hi")) // the above is equavalent to the following, and x is ignored
         // 1.3) Wildcard patterns
-        Some(5) match {                     // i.e. val Some(x) = Some.apply(5), where x is ignored and replaced with _
+        Some(5) match {
             case Some(_) => println("Yes")
+        }
+        Some(5) match {                     // the above is equavalent to the following 
+            case Some(x) => println("Yes")  // i.e. val Some(x) = Some.apply(5), and x is ignored (replaced with _)
         }
         // 2) Placeholders
         // 2.1) Partially applied functions
-        List(1, 2, 3).foreach(println(_))   // lambda expression: when writing lambda with no explicit argument type, Scala infers type from argument that foreach expects
-        List(1, 2, 3).foreach(              // so the above is equavalent to the following
+        List(1, 2, 3).foreach(println(_))   // lambda expression: writing lambda with no explicit argument type
+                                            //   compiler will infer type from argument that foreach expects
+        List(1, 2, 3).foreach(              // the above is equavalent to the following
             { (x: Int) => println(x) }
         )
-        List(1, 2, 3).foreach(println _)    // "process _" represents a function/method
         // 2.2) Placeholder syntax
-        println(List(1, 2, 3).map(_ + 1))
+        println(List(1, 2, 3).map(_ + 1))   // _ + 1 represents a lambda expression
+        println(List(1, 2, 3).map(          // the above is equavalent to the following
+            { (x: Int) => (x + 1) }
+        ))
 
         // 3) Wildcards
         // 3.1) Wildcard imports
         import java.util._
-        // 
     }
 }
