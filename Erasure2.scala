1b73cc757a95e8dcf7e9f929b80a11886401262e
kuanyu <starmap0312@gmail.com>
2017-03-06 18:45:56 +0800

    add more

--- Erasure2.scala
+++ Erasure2.scala
@@ -16,6 +16,7 @@ object Erasure2 {
             // warning: type pattern List[String] is unchecked since it is eliminated by erasure
             // (the above can be viewed as the following )
             // case Success(list: List)      => println("Success: total length of strings in the list = " + list.map(_.size).sum)
+            // so you lose the ability to distinguish if the passed-in list is of type List[String]
             case _                           => println("No match")
         }
         handle(Failure("error"))
@@ -23,7 +24,7 @@ object Erasure2 {
         // handle(Success(List("a", "b", 3))) // runtime exception: ClassCastException: Integer cannot be cast to String
 
         // solution: type checked, type mismatch error received at compile time
-        // (assume that we have the permission to edit the function directly)
+        // (this assumes that we can control what type of value gets passed to function handle())
         trait Result2[T]
         case class Success2[T](list: List[T]) extends Result2[T]
         case class Failure2[T](msg: String)   extends Result2[T]
@@ -35,5 +36,32 @@ object Erasure2 {
         handle2(Failure2("error"))
         handle2(Success2(List("a", "b", "cd")))
         //handle2(Success2(List("a", "b", 3))) // compile error: type mismatch, found: Int(3) but required: String
+        // why is it good?
+        // 1) we enforce boundaries to the list type included in the Success2 type at compile time
+        // 2) we make the type parameter covariant
+
+        // problem: what if we cannot control what type of value gets passed to function handle()
+        // ex. Akka receive() function always accept messages of type Any
+        // solution: do not use type parameter in the pattern matching function; instead, define explict types for compiler
+        // (this assumes that we can control the body of the pattern matching function)
+        def handle3(x: Any): Unit = x match {
+            case list: List[String] => println("Any lists matched: total length of strings in the list = " + list.map(_.size).sum)
+            case _                  => println("No match")
+        }
+        handle3(List("a", "b", "cd"))
+        // handle3(List("a", "b", 3))          // runtime exception: ClassCastException: Integer cannot be cast to String
+        // solution: do not use type parameter in the pattern matching function; instead, define explict types for compiler
+        // (this assumes that we can control the body of the pattern matching function)
+        case class Strings(values: List[String])
+        def handle4(x: Any): Unit = x match {
+            case Strings(list)      => println("String lists matched: total length of strings in the list = " + list.map(_.size).sum)
+            case _                  => println("No match")
+        }
+        handle4(Strings(List("a", "b", "cd")))
+        //handle4(Strings(List("a", "b", 3)))  // compile error: type mismatch, found: Int(3), required: String
+        // why is it good?
+        // 1) we define explict type Strings to manage the generic types for us
+        //    this ensures that you cannot build lists of mixed types
+        // 2) handle4() function can safely use lists without having to know the type parameters of lists at runtime
     }
 }
