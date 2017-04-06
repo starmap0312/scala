d37bb86727f15cb33c8a9b4fe326aea4f4395886
kuanyu <starmap0312@gmail.com>
2017-04-06 15:05:41 +0800

    add more

--- Implicit.scala
+++ Implicit.scala
@@ -6,7 +6,7 @@ object Implicit {
         //         if there is no implicit value of the right type in scope, it will not compile
         implicit val myX = 2
         def multiply(x: Int)(implicit y: Int) = x * y
-        println(multiply(3)(4))                       // a value for the implicit parameter is supplied 
+        println(multiply(3)(4))                       // a value for the implicit parameter is supplied, i.e. takes implicit myX from the current scope
         println(multiply(3))                          // no value is supplied, so an implicit variable of the right type must be found in the scope
 
         // 2) implicit classes:
@@ -25,13 +25,29 @@ object Implicit {
         // to use this class, just import it into scope and call the times method
         // ex.
         3 times print("Hi ")                                               // Hi Hi Hi 
+        println
         // implicit classes have the following restrictions:
-        // 1) they must be defined inside of another trait/class/object
-        // 2) they may only take one non-implicit argument in their constructor
+        // a) they must be defined inside of another trait/class/object
+        // b) they may only take one non-implicit argument in their constructor
         //    ex. implicit class RichDate(date: java.util.Date)            // OK
         //        implicit class Indexer[T](collecton: Seq[T], index: Int) // NOT OK
-        // 3) there may not be any method, member or object in scope with the same name as the implicit class
+        // c) there may not be any method, member or object in scope with the same name as the implicit class
         //    this means an implicit class cannot be a case class (because there will be a companion object created automatically if defined as a case class)
         //    ex. implicit case class Baz(x: Int)                          // NOT OK
+
+        // 3) implicit scope of an argument's type
+        //    if you have a method with an argument type A, then the implicit scope of type A will also be considered
+        //      "implicit scope" means all these rules will be applied recursively
+        //      ex. the companion object of A will be searched for implicits, as per the rule above
+        class A(val n: Int) {
+            def +(other: A) = new A(n + other.n)
+        }
+        object A {
+            implicit def fromInt(n: Int) = new A(n)
+        }
+        // This becomes possible:
+        1 + new A(1)
+        // because it is converted into this:
+        A.fromInt(1) + new A(1)
     }
 }
