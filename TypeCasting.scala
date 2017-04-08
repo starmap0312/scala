a11e6bd24e78bb1cba94c94831b2cdbbfa0bb32e
kuanyu <starmap0312@gmail.com>
2017-04-08 13:41:03 +0800

    add more

--- TypeCasting.scala
+++ TypeCasting.scala
@@ -1,33 +1,47 @@
-// isInstanceOf/asInstanceOf/classOf
+// isInstanceOf[T]/asInstanceOf[T]
+// classOf[T]
 object TypeCasting {
 
     def main(args: Array[String]) {
-        // 1) isInstanceOf[T]: check if object is an instance of type T
+        // 1) isInstanceOf[T]: check if object is an instance of calss T
+        println("string".isInstanceOf[String])            // true
+        println(1.isInstanceOf[Double])                   // false 
         class Foo
         class Bar extends Foo
         val foo = new Foo   
         val bar = new Bar 
         println(foo.isInstanceOf[Foo])                    // true
         println(bar.isInstanceOf[Foo])                    // true
-        println("string".isInstanceOf[String])            // true
-        println(1.isInstanceOf[Double])                   // false 
 
         // 2) asInstanceOf[T]: cast an instance to type T (a runtime operation)
-        //                     compiler believes that the type can be casted (it throws ClassCastException at runtime if casting fails) 
+        //                     compiler believes that the type can be casted at compile-time
+        //                     it throws ClassCastException if casting fails at runtime
         println(1.asInstanceOf[Double])                   // 1.0: cast Int to Double at runtime
-        //println(1.asInstanceOf[String])                 // ClassCastException at runtime
+        //println(1.asInstanceOf[String])                 // throws ClassCastException at runtime
         // erasure problem: as type argument is erased as part of compilation
-        //                  it is not possible to check whether the list are of requested type
-        println(List(1, 2, 3).asInstanceOf[List[String]]) // not working as parameterized type [String] will be eliminated by erasure
-        println(List(1, 2, 3).asInstanceOf[List[_]])      // the above is interpreted by compiler as 
+        //                  it is not possible to check whether a List is of a requested type
+        println(List(1).asInstanceOf[List[String]])       // not working as type parameter is eliminated by erasure
+        // the above is interpreted by compiler as the following 
+        println(List(1).asInstanceOf[List[_]])
 
-        // 3) classOf[T]: returns a runtime representation of the Scala class type T
-        //    def classOf[T]: java.lang.Class[T]          // i.e. a value of type Class[T] 
-        //      it retrieves the runtime representation of a class type
-        //      classOf[T] is equivalent to the class literal T.class in Java
-        println(classOf[String])                          // class java.lang.String
-        println(classOf[Array[String]])                   // class [Ljava.lang.String;
-        println(classOf[Double])                          // double 
-        //println(classOf[Double].cast(1))                // ClassCastException: Cannot cast java.lang.Integer to double
+        // 3) classOf[T]: returns a runtime type representation of a Scala class
+        //    def classOf[T]: Class[T]                    // returns a value of type java.lang.Class[T] 
+        //    note: classOf[T] is equivalent to the class literal T.class in Java
+        println(classOf[Number])                          // a Class[Number] instance, i.e. a Number type representation
+        println(classOf[String])                          // a Class[String] instance, i.e. a String type representation
+        println(classOf[List[String]])                    // a Class[List[String]] instance, i.e. a List type representation 
+        // 3.1) isInstance(obj): Boolean
+        //      if ctag = classOf[T], ctag.isInstance(obj) == obj.isInstanceOf[T]
+        //      determines if the specified Object is assignment-compatible with the object represented by this Class
+        //      this method is the dynamic equivalent of the Java language instanceof operator
+        println(classOf[Number].isInstance(1))            // true, check if an object is an instance of class Number
+        println(classOf[String].isInstance(1))            // false, check if an object is an instance of class String 
+        println(classOf[List[String]].isInstance(List(1)))// true, check if an object is an instance of class List 
+        // 3.2) cast(obj): T
+        //      if ctag = classOf[T], ctag.cast(obj) == obj.asInstanceOf[T]
+        //      Casts an object to the class or interface represented by this Class object
+        println(classOf[Number].cast(1))                  // cast object 1 into Number class 
+        //println(classOf[Number].cast("a"))              // ClassCastException: Cannot cast String to Number 
+        //println("a".asInstanceOf[Number])               // ClassCastException: Cannot cast String to Number 
     }
 }
