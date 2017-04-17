7308f5c18adac15c29dd26d4fff65f552854592c
kuanyu <starmap0312@gmail.com>
2017-04-17 18:13:26 +0800

    add more

---
+++ matching_objects_with_patterns/OODecomposition.scala
@@ -0,0 +1,44 @@
+object OODecomposition {
+    // class hierarchy
+    trait Expr {
+        def isNum: Boolean = false                         // test methods return false by default
+        def isVar: Boolean = false
+        def isMul: Boolean = false
+        def value: Int     = throw new NotImplementedError // accessor methods
+        def name : String  = throw new NotImplementedError
+        def left:  Expr    = throw new NotImplementedError
+        def right: Expr    = throw new NotImplementedError
+    }
+
+    // each subclass re-implements its own accessor method and test method
+    class Num(override val value: Int)   extends Expr {     // override val: unify class constructor and overriding accessor method as one
+        override def isNum = true
+    }
+
+    class Var(override val name: String) extends Expr {
+        override def isVar = true
+    }
+
+    class Mul(override val left: Expr, override val right: Expr) extends Expr {
+        override def isMul = true
+    }
+
+    // simplification rule
+    def simplify(expr: Expr) = {
+        if (expr.isMul) {                                 // use test method is distinguish match patterns
+            val r = expr.right                            // use accessor method to get the public field
+            if (r.isNum && r.value == 1) {
+                expr.left
+            } else {
+                expr
+            }
+        } else {
+            expr
+        }
+    }
+
+    def main(args: Array[String]) {
+        val expr = new Mul(new Num(21), new Num(1))
+        assert(simplify(expr).value == 21)
+    }
+}
---
+++ matching_objects_with_patterns/OOVisitors.scala
@@ -0,0 +1,51 @@
+object OOVisitors {
+    // class hierarchy
+    trait Visitor[T] {
+        def caseNum(t:  Num): T = default(t)
+        def caseVar(t:  Var): T = default(t)
+        def caseMul(t:  Mul): T = default(t)
+        def default(t: Expr): T = throw new MatchError(t)
+    }
+
+    trait Expr {
+        def matchWith[T](v: Visitor[T]): T
+    }
+
+    class Num(val value: Int)   extends Expr {
+        def matchWith[T](v: Visitor[T]): T = v.caseNum(this)
+    }
+
+    class Var(val name: String) extends Expr {
+        def matchWith[T](v: Visitor[T]): T = v.caseVar(this)
+    }
+
+    class Mul(val left: Expr, val right: Expr) extends Expr {
+        def matchWith[T](v: Visitor[T]): T = v.caseMul(this)
+    }
+
+    // simplification rule
+    def simplify(expr: Expr) = {
+        expr.matchWith {
+            new Visitor[Expr] {
+                override def caseMul(mul: Mul) = mul.right.matchWith {
+                    new Visitor[Expr] {
+                        override def caseNum(num: Num) = {
+                            if (num.value == 1) {
+                                mul.left
+                            } else {
+                                expr
+                            }
+                        }
+                        override def default(e: Expr) = e
+                    }
+                }
+                override def default(e: Expr) = e
+            }
+        }
+    }
+
+    def main(args: Array[String]) {
+        val expr = new Mul(new Num(21), new Num(1))
+        assert(simplify(expr).asInstanceOf[Num].value == 21)
+    }
+}
