4822b19c23d3e4b34d585070eda124740846f0df
kuanyu <starmap0312@gmail.com>
2017-04-18 10:30:40 +0800

    add more

---
+++ matching_objects_with_patterns/CaseClasses.scala
@@ -0,0 +1,25 @@
+// use of Case Classes
+// 1) case class
+//    Scala provides convenient shorthands for constructing and analyzing data
+// 2) the case modifier has several effects:
+//    apply()  : convenient for constructing data without having to write new
+//    unapply(): it allows pattern matching on their constructor
+//               they are written exactly like constructor expressions, but are interpreted in reverse (i.e. extractors)
+object CaseClasses {
+    // Class hierarchy:
+    trait Expr
+    case class Num(val value: Int)                  extends Expr
+    case class Var(val name: String)                extends Expr
+    case class Mul(val left: Expr, val right: Expr) extends Expr
+
+    // Simplification rule:
+    def simplify(expr: Expr) = expr match {
+        case Mul(x, Num(1)) => x
+        case _ => expr
+    }
+
+    def main(args: Array[String]) {
+        val expr = Mul(Num(21), Num(1)) // i.e. val expr = new Mul(new Num(21), new Num(1))
+        assert(simplify(expr).asInstanceOf[Num].value == 21)
+    }
+}
