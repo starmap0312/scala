f28d2f209cd8ac20698d45b4e440d95cfccbeb4e
kuanyu <starmap0312@gmail.com>
2017-10-12 13:59:40 +0800

    add more

--- TrySuccessFailure.scala
+++ TrySuccessFailure.scala
@@ -2,7 +2,7 @@ import scala.util.{Try, Success, Failure}
 
 object TrySuccessFailure {
 
-    def bad(num: Int): Try[Int] = {
+    def tryException(num: Int): Try[Int] = {
         // Try({expression}) will wrap the successfully evaluated value or any exception thrown
         //   a way to handle the error late and continue to pass it on to the pipeline (operation chain)
         Try(                                 // return object wrapped in Success or Failure
@@ -16,16 +16,20 @@ object TrySuccessFailure {
 
     def main(args: Array[String]) {
         // both Success[T] and Failure[T] extend Try[T]
-        bad(3) match {
+        tryException(3) match {
             case Success(num) => println(s"Success: $num")           // Success: 3
             case Failure(ex)  => println(s"Failure: ${ex.getClass}")
         }
-        bad(4) match {
+        tryException(4) match {
             case Success(num) => println(s"Success: $num")
             case Failure(ex)  => println(s"Failure: ${ex.getClass}") // Failure: class java.lang.Exception
         }
         val tryString: Try[String] = Try("one")
         //val tryString: Try[String] = Try(1)                        // compile-time error: type mismatch
         println(tryString)                                           // Success(string)
+
+        // 1) try.get(), try.getOrElse([default value]):
+        println(Try(1).get)                             // 1
+        println(Try(throw new Exception).getOrElse(-1)) // -1
     }
 }
