ae42a3b537083de2615fd3c052036c5690e390ab
kuanyu <starmap0312@gmail.com>
2018-02-10 18:30:39 +0800

    add more

--- functional_programming_principles_in_scala/2_week1.scala
+++ functional_programming_principles_in_scala/2_week1.scala
@@ -16,4 +16,41 @@
 3) object-oriented programming
 
 // Elements of Programming
+1) side effect
+   ex. i++
+       it has side effect, as variable i is incremented by one (mutated)
+2) does every expression evaluate to a value in a finite number of steps? No.
+   ex. 
+   def loop: Int = loop // (loop -> loop -> ... ...)
+3) evaluation strategies: there are more than one way to evaluate an expression
+   call-by-value vs. call-by-name (lazy evaluation)
+   ex.
+   (call-by-value: it evaluates function argument only once)
+     sumOfSquare(3, 2 + 2) =  sumOfSquare(3, 4) = square(3) + square(4) = (3 * 3) + square(4) = 9 + square(4)
+       = 9 + (4 * 4) = 9 + 16 = 25
+   (call-by-name: a function argument is not evaluated if it is not used in the function body)
+     sumOfSquare(3, 2 + 2) =  square(3) + square(2 + 2) = (3 * 3) + square(2 + 2) = 9 + square(2 + 2)
+       = 9 + (2 + 2) * (2 + 2) = 9 + 4 * (2 + 2) = 9 + 4 * 4 = 9 + 16 = 25
+4) theorem of lamda-calculus:
+   both strategies evaluate to the same final value as long as they consists of pure functions (no side effect) and both terminate
 
+// Evaluation Strategies and Terminations
+1) if call-by-value terminates, then call-by-name terminates too, however,
+   if call-by-name terminates, then it does not imply call-by-value terminates
+   ex.
+     def first(x: Int, y: Int) = x
+     first(1, loop) // call-by-value will not terminate, as evaluating loop does not terminate (call-by-name does not evaluate loop)
+
+// Conditionals and Value Definitions
+1) conditional expression in scala
+   ex.
+     def abs(x: Int) = if (x >= 0) x else -x
+2) def form is by-name
+   val form is by-value
+
+// Example
+1) a recursive method must have an explicit return type (otherwise, the compiler cannot infer its type)
+   ex.
+     def sqrtIter(guess: Double, x: Doulbe): Double = // return type must be explcit for recursive methods
+       if (isGoodEnough(guess, x)) guess
+       else sqrtIter(improve(guess, x), x)
