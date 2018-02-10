d8da1d46aa6263e788c5da77da0f93e37d360d9a
kuanyu <starmap0312@gmail.com>
2018-02-10 17:30:31 +0800

    add more

---
+++ functional_programming_principles_in_scala/2_week1.scala
@@ -0,0 +1,19 @@
+// Programming Paradigms
+1) imperative programming
+   variables assignments, control structures (if-else branch, loops, etc.)
+2) functional programming (declaritive)
+   no mutations, operators/functions
+   in a restricted sense: no mutable variables, assignments, control structures
+     ex. Haskell without I/O Monad or UnsafePerformIO
+   in a wider sense     : contruct programs using functions
+     ex. Haskell in full , or Scala
+     functions are first-class citizens, i.e. they can be passed as parameters or as returned results
+     there are a set of operators to compose functions
+   pros:
+     simple reasoning principles, i.e. good modularity
+     good for parallelism, i.e. for multicores (ex. akka) and for distributed systems (ex. spark)
+(orthogonal to OOP, both of the above two can be combined with OOP)
+3) object-oriented programming
+
+// Elements of Programming
+
